8c5e6313ad1ffee36bd49c263a425df08151c85b
Richard Rabe <Richard Rabe>
2016-10-27 00:01:43 +0200

    added configs

---
+++ config/fish/config.fish
@@ -0,0 +1,10 @@
+tty-clock -c -C 1
+
+# COLOURED MANPAGES
+set -x LESS_TERMCAP_mb (printf "\033[01;31m")  
+set -x LESS_TERMCAP_md (printf "\033[01;31m")  
+set -x LESS_TERMCAP_me (printf "\033[0m")  
+set -x LESS_TERMCAP_se (printf "\033[0m")  
+set -x LESS_TERMCAP_so (printf "\033[01;44;33m")  
+set -x LESS_TERMCAP_ue (printf "\033[0m")  
+set -x LESS_TERMCAP_us (printf "\033[01;32m")
